3a042ce6a2332e88005599a83db5c64548d9f715
sashaPEn <88935397+sashaPEn@users.noreply.github.com>
2022-05-02 16:58:30 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,12 +5,12 @@ import Layout from "../components/layout"
 import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
-    <Seo title="Home" />
+    <Seo title="Домашние задание" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <StaticImage
-      src="../images/gatsby-astronaut.png"
+      src="../images/icon-removebg-preview.png"
       width={300}
       quality={95}
       formats={["auto", "webp", "avif"]}
